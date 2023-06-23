aed3d85e17927dd8e2cee748dc06f767a1285dd0
Timur Nizamov <abc@nizamovtimur.ru>
2023-06-23 21:43:33 +0500

    we have finally decided use bert

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from fastapi import FastAPI
-from schemas import AnnotatedVacancy
+from razdel import sentenize
+import torch
+from transformers import pipeline
 
 app = FastAPI()
 
+device = torch.cuda.current_device() if torch.cuda.is_available() and torch.cuda.mem_get_info()[0] >= 2*1024**3 else -1
+model = pipeline("text-classification", "model", device=device)
+
 
 @app.get("/")
-async def index():
-    return {"message": "Hello World"}
-
-
-@app.post("/annotation", status_code=200)
-async def annotation(raw_vacancy: str) -> AnnotatedVacancy:
-    return AnnotatedVacancy(
-        responsobilities="responsobilities",
-        requirements="requirements",
-        terms="terms",
-        skills="skills",
-        notes="notes"
-    )
+async def index(text: str):
+    result = {"responsibilitie": "",
+              "requirement": "",
+              "terms": "",
+              "notes": ""}
+    text = text.replace("\n", " ").replace("\t", " ").replace("\r", "")
+    sentences = [sentence.text for sentence in sentenize(text)]
+    predicts = [predict["label"] for predict in model.predict(sentences)]
+    for sentence, label in zip(sentences, predicts):
+        result[label.lower()] += sentence + " "
+    return result
