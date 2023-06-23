d826ebebbc9803a05551c9a9232598aa155d073c
daniil7 <daniil.k@mail.ru>
2023-06-23 22:43:35 +0500

    fix one-format text processing

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -5,6 +5,22 @@ from xlutils.copy import copy
 
 import json
 
+def get_front_color(xf, wb):
+    font = wb.font_list[xf.font_index]
+    if not font:
+        return None
+    return get_color(font.colour_index, wb)
+def get_back_color(xf, wb):
+    if not xf.background:
+        return None
+    return get_color(xf.background.background_colour_index, wb)
+def get_color(color_index, wb):
+    return wb.colour_map.get(color_index)
+def get_if_protected(xf):
+    if not xf.protection:
+        return False
+    return xf.protection.cell_locked
+
 COL_IDX = 3
 
 wb = xlrd.open_workbook('data/Датасет.xls', 'rw', formatting_info=True)
@@ -42,8 +58,12 @@ for row_idx in range(1, wb_sheet.nrows):
                         text_cell = text_cell[:offset-deleted] + text_cell[next_offset-deleted:]
                         deleted += next_offset-offset
     else:
-        # TODO
-        #text_cell = u''
+        c = wb_sheet.cell(row_idx, COL_IDX)
+        xf = wb.xf_list[c.xf_index]
+        color = get_front_color(xf, wb)
+        if (color is not None):
+            if (color[0] == 0 and color[1] == 176 and color[2] == 80):
+                text_cell = u''
 
     wb_w_sheet.write(row_idx, 0, original_responsibilities) # original responsibilities
     wb_w_sheet.write(row_idx, 1, text_cell) # cleaned responsibilities
