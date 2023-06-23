f434e0e04ccbdae029f05caa1d27ce8b37a53526
daniil7 <daniil.k@mail.ru>
2023-06-23 22:18:10 +0500

    fix

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -15,13 +15,14 @@ wb_w_sheet = wb_w.add_sheet('sheet1')
 
 wb_w_sheet.write(0,0,u'text')
 wb_w_sheet.write(0,1,u'responsibilities')
-wb_w_sheet.write(0,2,u'requirements')
-wb_w_sheet.write(0,3,u'terns')
-wb_w_sheet.write(0,4,u'notes')
+wb_w_sheet.write(0,2,u'modified')
+wb_w_sheet.write(0,3,u'requirements')
+wb_w_sheet.write(0,4,u'terns')
+wb_w_sheet.write(0,5,u'notes')
 
 for row_idx in range(1, wb_sheet.nrows):
     text_cell = wb_sheet.cell(row_idx, COL_IDX).value
-    wb_w_sheet.write(row_idx, 0, text_cell)
+    original_responsibilities = text_cell
     text_cell_xf = wb.xf_list[wb_sheet.cell_xf_index(row_idx, COL_IDX)]
     if not text_cell:
         continue
@@ -40,9 +41,16 @@ for row_idx in range(1, wb_sheet.nrows):
                         next_offset = text_cell_runlist[segment_idx+1][0]
                         text_cell = text_cell[:offset-deleted] + text_cell[next_offset-deleted:]
                         deleted += next_offset-offset
-    wb_w_sheet.write(row_idx, 1, text_cell)
-    wb_w_sheet.write(row_idx, 2, wb_sheet.cell(row_idx, 4).value)
-    wb_w_sheet.write(row_idx, 3, wb_sheet.cell(row_idx, 5).value)
-    wb_w_sheet.write(row_idx, 4, wb_sheet.cell(row_idx, 16).value)
+    else:
+        # TODO
+        #text_cell = u''
+
+    wb_w_sheet.write(row_idx, 0, original_responsibilities) # original responsibilities
+    wb_w_sheet.write(row_idx, 1, text_cell) # cleaned responsibilities
+    wb_w_sheet.write(row_idx, 2, 0 if text_cell == original_responsibilities else 1) # is green text has been found
+    wb_w_sheet.write(row_idx, 3, wb_sheet.cell(row_idx, 4).value) # requirements
+    wb_w_sheet.write(row_idx, 4, wb_sheet.cell(row_idx, 5).value) # terns
+    notes = wb_sheet.cell(row_idx, 16).value
+    wb_w_sheet.write(row_idx, 5, u"" if notes == u"\\N" else notes) # notes
 
 wb_w.save('data/Датасет_обработанный.xls')
