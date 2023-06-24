858fe88e0708b71e8264843abf1e336d30057ace
Arseniy <seno@moad.dev>
2023-06-24 21:36:02 +0500

    train model on augumented data

--- save_model.py
+++ save_model.py
@@ -1,5 +1,5 @@
 from transformers import pipeline
 
-model_checkpoint = "seninoseno/rubert-tiny-vacancy-information-extractor"
+model_checkpoint = "seninoseno/rubert-tiny-vacancy-information-extractor-augumented"
 model = pipeline(model=model_checkpoint)
 model.save_pretrained("extractor_model")
