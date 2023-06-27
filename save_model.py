78afd9ad40f6629a72dcbcd2126465a8d540d554
SeninoSeno <arseniy2002@mail.ru>
2023-06-27 20:48:38 +0500

    add hack result at README.md + minor changes

--- save_model.py
+++ save_model.py
@@ -1,5 +1,5 @@
 from transformers import pipeline
 
-model_checkpoint = "seninoseno/rubert-tiny-vacancy-information-extractor-augumented"
+model_checkpoint = "seninoseno/rubert-tiny-vacancy-information-extractor"
 model = pipeline(model=model_checkpoint)
 model.save_pretrained("extractor_model")
